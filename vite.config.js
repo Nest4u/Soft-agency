8d27b75d178a665262f62f28784ec25f9005e53a
Nest <58371847+Nest4u@users.noreply.github.com>
2024-04-17 15:02:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Soft-agency/',
+ 
+  
   
   plugins: [react()],
   base: '/Soft-agency/',
